07889aea17347219e5ca21a14c4c7f774ecf7544
Camille Leroux <camille.leroux@ims-bordeaux.fr>
2026-03-31 15:09:28 +0200

    Refacto hpp/cpp.

--- Common/streampu/sw/Comparator.cpp
+++ Common/streampu/sw/Comparator.cpp
@@ -1,37 +1,46 @@
-#include <random>
 #include "Comparator.hpp"
 
 using namespace spu;
 using namespace spu::module;
 
-    Comparator::Comparator(int frame_size) : Stateful(), frame_size(frame_size) {
+Comparator::Comparator(int frame_size)
+    : Stateful(), frame_size(frame_size), n_errors_total(0), n_frames(0)
+{
+    this->set_name("Comparator");
+    this->set_short_name("Comparator");
 
-        this->set_name("Comparator");
-        this->set_short_name("Comparator");
+    auto &t    = this->create_task("check");
+    auto p_ref = this->create_socket_in<int>(t, "in_ref", frame_size);
+    auto p_got = this->create_socket_in<int>(t, "in_got", frame_size);
 
-        auto &t = create_task("compare");
-        auto input1   = create_socket_in<int>(t, "input1", frame_size);
-        auto input2   = create_socket_in<int>(t, "input2", frame_size);
-        auto output   = create_socket_out<int>(t, "output", frame_size);
+    this->create_codelet(t, [p_ref, p_got](Module &m, runtime::Task &t, const size_t /*frame_id*/) -> int {
+        auto &self     = static_cast<Comparator&>(m);
+        const int* ref = static_cast<const int*>(t[p_ref].get_dataptr());
+        const int* got = static_cast<const int*>(t[p_got].get_dataptr());
 
-        this->create_codelet(t, [input1, input2, output](Module &m, runtime::Task &t, const size_t frame_id) -> int {
-        static_cast<Comparator&>(m).compare(   static_cast<int*>(t[input1].get_dataptr()),
-                                                static_cast<int*>(t[input2].get_dataptr()),
-                                                static_cast<int*>(t[output].get_dataptr()),
-                                                        frame_id);
-        return 0;
+        int errors = 0;
+        for (int i = 0; i < self.frame_size; i++)
+            if (ref[i] != got[i]) errors++;
+
+        self.n_errors_total += errors;
+        self.n_frames++;
+        return runtime::status_t::SUCCESS;
     });
+}
 
-    }
+Comparator::~Comparator() = default;
 
-    Comparator::~Comparator() {
-    }    
+int Comparator::get_n_errors() const
+{
+    return n_errors_total;
+}
 
-    void Comparator::compare(int *input1, int *input2, int *output, const int frame_id) {
-        
-        for(auto i = 0; i < frame_size; i++)
-        {
-            if(input1[i] == input2[i]) output[i] = 0;
-            else output[i] = 1;            
-        }        
-    }
+int Comparator::get_frame_count() const
+{
+    return n_frames;
+}
+
+bool Comparator::has_errors() const
+{
+    return n_errors_total > 0;
+}
--- Common/streampu/sw/Comparator.hpp
+++ Common/streampu/sw/Comparator.hpp
@@ -21,36 +21,12 @@ private:
     int n_frames;
 
 public:
-    Comparator(int frame_size)
-        : Stateful(), frame_size(frame_size), n_errors_total(0), n_frames(0)
-    {
-        this->set_name("Comparator");
-        this->set_short_name("Comparator");
+    Comparator(int frame_size);
+    virtual ~Comparator();
 
-        auto &t    = this->create_task("check");
-        auto p_ref = this->create_socket_in<int>(t, "in_ref", frame_size);
-        auto p_got = this->create_socket_in<int>(t, "in_got", frame_size);
-
-        this->create_codelet(t, [p_ref, p_got](Module &m, runtime::Task &t, const size_t /*frame_id*/) -> int {
-            auto &self       = static_cast<Comparator&>(m);
-            const int* ref   = static_cast<const int*>(t[p_ref].get_dataptr());
-            const int* got   = static_cast<const int*>(t[p_got].get_dataptr());
-
-            int errors = 0;
-            for (int i = 0; i < self.frame_size; i++)
-                if (ref[i] != got[i]) errors++;
-
-            self.n_errors_total += errors;
-            self.n_frames++;
-            return runtime::status_t::SUCCESS;
-        });
-    }
-
-    virtual ~Comparator() = default;
-
-    int  get_n_errors()    const { return n_errors_total; }
-    int  get_frame_count()  const { return n_frames;       }
-    bool has_errors()       const { return n_errors_total > 0; }
+    int  get_n_errors() const;
+    int  get_frame_count() const;
+    bool has_errors() const;
 };
 
 } // namespace module
